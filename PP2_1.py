3ed6873ca8f66ac70671394684f267dbafce026c
Andy-Chow-ICD2O1-02 <andy.chow2@student.tdsb.on.ca>
2024-10-09 18:32:13 +0000

    finished assignment

--- PP2_1.py
+++ PP2_1.py
@@ -1,10 +1,21 @@
 
 
 def q1(): 
+    num = input("In: ")
+    num = int(num)
+    if not num%2:
+        print(f"{num} is even")
+    else: 
+        print(f"{num} is odd")
   #Write Assignment code here
 
 
 def q2(): 
+    name = input("In: ")
+    if name == "Kalisz":
+        print("teacher")
+    else:
+        print("student")
   #Write Assignment code here
 
 
@@ -12,5 +23,5 @@ def q2():
 #Do not alter the following code
 #Comment out the following code when running your tests
 
-q1()
-q2()
+# q1()
+# q2()
